32a61ade9b34e8cd29820a40612a8f4da1273983
Coach Beard Jr. <65058877+vijay2249@users.noreply.github.com>
2024-12-16 19:53:38 +0530

    Update utils/main.go
    failing to read folders, need to check

--- utils/main.go
+++ utils/main.go
@@ -1,7 +1,7 @@
 package utils
 
 import (
-	"fmt"
+	//"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -13,15 +13,40 @@ import (
 	constant "github.com/wedonttrack.org/vproxie/constant"
 )
 
-var ENV_DIR_RELATIVE_PATH string = "../.env/"
+var ENV_DIR_RELATIVE_PATH string = "..\\.env\\"
+
+func getEnvDirPath() (string, error) {
+	path, err := os.Getwd()
+
+	if err != nil {
+		log.Fatal("Unable to get the current working directory location")
+		return "", err
+	}
+
+	path += "\\" + ENV_DIR_RELATIVE_PATH
+
+	//check if folder exists or not
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		log.Fatalf("required folder/folders (%v) are not present", path)
+		return "", err
+	}
+	
+	return path, nil
+}
 
 func getAllEnvFiles() ([]string, error) {
 	var fileNames []string
-	//TODO - error in reading file - fix it
-	err := filepath.Walk(ENV_DIR_RELATIVE_PATH, func(path string, info os.FileInfo, err error) error{
+
+	envFolderPath, err := getEnvDirPath()
+	if err != nil  || envFolderPath == "" {
+		log.Fatal("Some error while getting env files")
+		return nil, err
+	}
+
+	err = filepath.Walk(envFolderPath, func(path string, info os.FileInfo, err error) error{
 		if err != nil {
-			//log.Panicf("unable to read file: %s", path)
-			fmt.Println(err)
+			log.Panicf("unable to read file: %s", path)
+			//fmt.Println(err)
 			return err
 		}
 		fileNames = append(fileNames, path)
@@ -57,7 +82,7 @@ func init(){
 	log.Println("========== ENV VALUES ==============")
 }
 
-func ModifyHeaders(reqHeaders *http.Header){
+func ModifyHeaders(reqHeaders *http.Header) {
 	for key := range *reqHeaders {
 		if slices.Contains(constant.HEADERS_TO_EXCLUDE, strings.ToLower(key)){
 			reqHeaders.Del(key)
@@ -67,11 +92,12 @@ func ModifyHeaders(reqHeaders *http.Header){
 
 func PrintHeaders(req *http.Request){
 	for key, value := range req.Header {
-		fmt.Println(key, value)
+		log.Print(key, value)
+		//fmt.Println(key, value)
 	}
 }
 
-func CreateCorelationHeader(){}
+func CreateCorelationHeader(headers *http.Header) {}
 
 func SafetyCheck(){}
 
